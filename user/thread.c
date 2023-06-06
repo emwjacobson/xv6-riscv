1bd52bbe410aff8571a55f73ec6d35721d121cd5
Emerson Jacobson <emwjacobson@gmail.com>
2023-06-05 20:47:26 -0700

    Implemented what I imagine the library should look like?

---
+++ user/thread.c
@@ -0,0 +1,34 @@
+#include "thread.h"
+#include "user.h"
+#include "kernel/riscv.h"
+
+int thread_create(void *(startup_routine)(void*), void *arg) {
+    // Allocate user stack of PGSIZE bytes
+    void *stack = malloc(PGSIZE);
+
+    // Call clone() to create child
+    int ret = clone(stack);
+
+    if (ret == -1) { // Parent failed to clone. Child doesn't exist
+
+    } else if (ret == 0) { // Parent
+
+    } else { // Child
+        startup_routine(arg);
+        exit(0);
+    }
+}
+
+// https://gcc.gnu.org/onlinedocs/gcc-4.1.0/gcc/Atomic-Builtins.html
+
+void lock_init(struct lock_t* lock) {
+    lock->locked = 0;
+}
+
+void lock_acquire(struct lock_t* lock) {
+    while (__sync_lock_test_and_set(&(lock->locked), 1)) {}
+}
+
+void lock_release(struct lock_t* lock) {
+    __sync_lock_release(&(lock->locked));
+}
